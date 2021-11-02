2134cba1fd421b8a5072f09aaff741df2ef258fd
seongbin <kled@kakao.com>
2021-11-03 00:33:27 +0900

    fix .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {
+  useHistory,
+  useLocation,
+  Switch,
+  Route,
+  BrowserRouter,
+  Router,
+} from "react-router-dom";
+
+import HomePage from "./containers/HomePage";
+
 function App() {
-  return <div>let's go</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/"} component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
